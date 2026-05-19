eb93f9fb335c4c67aa3e4308979e7a79fb07e872
raouf21_dev <raouf_dev@hotmail.com>
2026-05-19 18:56:32 +0100

    add IMAGE_NAME

--- script.groovy
+++ script.groovy
@@ -15,9 +15,9 @@ def incrementVersion(){
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t santana20095/demo-app:jma-2.0 .'
+        sh "docker build -t santana20095/demo-app:${env.IMAGE_NAME} ."
         sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push santana20095/demo-app:jma-2.0'
+        sh "docker push santana20095/demo-app:${env.IMAGE_NAME}"
     }
 }
 
